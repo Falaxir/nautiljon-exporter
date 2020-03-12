6291d8a190e524bd12918f3787ad0721c612e6d9
Alexandre Zhan <alexandre.zhan@epita.fr>
2020-03-12 01:30:03 +0100

    exporter: print title

--- exporter.py
+++ exporter.py
@@ -2,12 +2,16 @@
 
 from bs4 import BeautifulSoup
 
-with open("test.html", "r") as f:
+with open("nautiliste_Dranixx.html", "r") as f:
     list_html = f.read()
 
 list_soup = BeautifulSoup(list_html, 'html.parser')
-print(list_soup.prettify())
 
-with open("print.html", "w") as w:
-    w.write(list_html)
-
+animes = list_soup.find_all(class_ = 'elt')
+count = 0
+for i in animes:
+    title = i.find(class_ = 't_titre')
+    title = title.find_all('a')
+    count += 1
+    print(str(count) + " " + title[0].contents[0])
+    
