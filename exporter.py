c97014792d20b467a73497937c9a8147f71dda32
Alexandre Zhan <alexandre.zhan@epita.fr>
2020-03-15 01:18:26 +0100

    add jikan search API

--- exporter.py
+++ exporter.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+from jikanpy import Jikan
 
-with open("nautiliste_Dranixx.html", "r") as f:
+with open("nautilist.html", "r") as f:
     list_html = f.read()
 
 list_soup = BeautifulSoup(list_html, 'html.parser')
-
 animes = list_soup.find_all(class_ = 'elt')
+
 count = 0
-for i in animes:
-    title = i.find(class_ = 't_titre')
-    title = title.find_all('a')
+jikan = Jikan()
+
+naut_type = {
+    "Série TV": "TV",
+    "OAV": "OVA",
+
+}
+
+for anime in animes:
     count += 1
-    print(str(count) + " " + title[0].contents[0])
+    
+    title = anime.find(class_ = 't_titre')
+    title = title.find_all('a')
+    title = title[0].contents[0]
+    print(str(count) + " - " + title)
+    
+    search = jikan.search('anime', title, parameters={'limit':'1'})
+    mal_id = search["results"][0]["mal_id"]
+    mal_title = search["results"][0]["title"]
+    print(str(mal_id) + " - " + mal_title)
     
