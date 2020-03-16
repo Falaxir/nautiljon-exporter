4ae217b4e269aa1ef2ade738131feb82165edbf2
Alexandre Zhan <alexandre.zhan@epita.fr>
2020-03-16 03:18:24 +0100

    exporter: add main()

--- exporter.py
+++ exporter.py
@@ -5,46 +5,56 @@ from jikanpy import Jikan
 import time
 import re
 
-with open("nautilist.html", "r") as f:
-    list_html = f.read()
 
-list_soup = BeautifulSoup(list_html, 'html.parser')
-animes = list_soup.find_all(class_ = 'elt')
+def main():
+    with open("test.html", "r") as f:
+        list_html = f.read()
 
-count = 0
-jikan = Jikan()
+    list_soup = BeautifulSoup(list_html, 'html.parser')
+    animes = list_soup.find_all(class_ = 'elt')
 
-nau_types = {
-    "Série TV": "tv",
-    "OAV": "ova",
-    "ONA": "ona",
-    "Film": "movie",
-    "Spécial": "special"
-}
+    count = 0
+    jikan = Jikan()
 
-for anime in animes:
-    count += 1
-    
-    # Nautilist
-    nau_title = anime.find(class_ = 't_titre')
-    nau_title = nau_title.find_all('a')
-    nau_title = nau_title[0].contents[0]
-    
-    nau_type = anime.find(class_ = 't_type')
-    nau_type = nau_type.find(class_ = 'format')
-    nau_type = nau_type.contents[0]
-    
-    # My anime list
-    search = jikan.search('anime', nau_title, parameters={'limit': '1', 'type': nau_types[nau_type]})
+    nau_types = {
+        "Série TV": "tv",
+        "OAV": "ova",
+        "ONA": "ona",
+        "Film": "movie",
+        "Spécial": "special"
+    }
 
-    if search["results"]:
-        mal_id = search["results"][0]["mal_id"]
-        mal_title = search["results"][0]["title"]
-        mal_type = search["results"][0]["type"]
-    
-        if nau_title.lower() != mal_title.lower():
-            print(str(count) + " - " + nau_title + " - " + nau_type, end = '')
-            print(" --> MAL: " + str(mal_id) + " - " + mal_title + " - " + mal_type)
+    for anime in animes:
+        count += 1
 
-    time.sleep(0.3)
-    
+        # Nautilist
+        nau_title = anime.find(class_ = 't_titre')
+        nau_title = nau_title.find_all('a')
+        nau_title = nau_title[0].contents[0]
+
+        nau_type = anime.find(class_ = 't_type')
+        nau_type = nau_type.find(class_ = 'format')
+        nau_type = nau_type.contents[0]
+
+        # My anime list
+        search = jikan.search('anime', nau_title, \
+                parameters={'limit': '1', 'type': nau_types[nau_type]})
+
+        if search["results"]:
+            mal_id = search["results"][0]["mal_id"]
+            mal_title = search["results"][0]["title"]
+            mal_type = search["results"][0]["type"]
+
+            nau_str = re.sub("[\s:]", "", nau_title.lower())
+            mal_str = re.sub("[\s:]", "", mal_title.lower())
+            if nau_str != mal_str:
+                print(str(count) + " - " + nau_title + " - " + nau_type, end = '')
+                print(" --> MAL: " + str(mal_id) + " - " + mal_title + " - " + mal_type)
+
+        if count % 2 == 0:
+            time.sleep(0.3)
+
+
+if __name__== "__main__":
+      main()
+
