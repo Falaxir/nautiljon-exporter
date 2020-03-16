389e676d369ca4fe67087a76f7405d3779103815
Alexandre Zhan <alexandre.zhan@epita.fr>
2020-03-16 03:41:37 +0100

    exporter: add progress bar

--- exporter.py
+++ exporter.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
+import re
+import sys
+import time
+
 from bs4 import BeautifulSoup
 from jikanpy import Jikan
-import time
-import re
 
 
 def main():
-    with open("test.html", "r") as f:
+    with open("nautilist.html", "r") as f:
         list_html = f.read()
 
     list_soup = BeautifulSoup(list_html, 'html.parser')
@@ -23,7 +25,7 @@ def main():
         "Film": "movie",
         "Spécial": "special"
     }
-
+    len_animes = len(animes)
     for anime in animes:
         count += 1
 
@@ -44,16 +46,28 @@ def main():
             mal_id = search["results"][0]["mal_id"]
             mal_title = search["results"][0]["title"]
             mal_type = search["results"][0]["type"]
-
+            
             nau_str = re.sub("[\s:]", "", nau_title.lower())
             mal_str = re.sub("[\s:]", "", mal_title.lower())
             if nau_str != mal_str:
                 print(str(count) + " - " + nau_title + " - " + nau_type, end = '')
-                print(" --> MAL: " + str(mal_id) + " - " + mal_title + " - " + mal_type)
-
+                print(" ==> MAL: " + str(mal_id) + " - " + mal_title + " - " + mal_type)
+        
+        #progress(count, len_animes, "Process animes list")    
         if count % 2 == 0:
+            # 2 requests per second max
             time.sleep(0.3)
 
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    
+    status = str(count) + '/' + str(total) + ' ' + status
+    sys.stdout.write('\r[%s] %s%s ... %s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
 if __name__== "__main__":
       main()
